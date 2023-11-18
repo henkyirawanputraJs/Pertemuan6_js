432b4a4dfd73563e6c1c4c5aefa129b62e8221f4
henkyirawanputraJs <henkyirawanputra6@gmail.com>
2023-11-17 20:48:43 -0800

    first commit

---
+++ ifelse.js
@@ -0,0 +1,13 @@
+const nilai = prompt('masukan nilai anda')
+if(nilai>90){
+    alert('grade A')
+}
+else if(nilai>80){
+    alert('grade B+ ')
+}
+else if(nilai>70){
+    alert('grade B')
+}else{
+    alert('grade F')
+}
+alert('Tingkatkan lagi belajarnya')
---
+++ index.js
@@ -0,0 +1,10 @@
+let kelamin ='Gay'
+if(kelamin==='Laki-laki'){
+    console.log('anda seorang Laki-laki ')
+}else if(kelamin==='Perempuan'){
+    console.log('anda seorang Perempuan')
+
+}else{
+    console.log('Mohon maaf gender anda masukan salah')
+}
+console.log(kelamin)
